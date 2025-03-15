c10d95abe2e85cc1a945b32f045a93816aa89c8a
Monika Kiskinova <monikakiskinova@gmail.com>
2025-03-15 13:23:19 +0200

    Show all  games

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,6 +1,12 @@
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const gameService = {
+    async getAll() {
+        const response = await fetch(baseUrl);
+        const result = await response.json();
+        const games = Object.values(result);
+        return games;
+    },
     async create(gameData) {
         const response = await fetch(baseUrl, {
             method: 'POST',
