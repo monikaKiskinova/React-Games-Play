e96d2f29fc3d30c7eeb0cfe34cd13c5cf7093a6a
Monika Kiskinova <monikakiskinova@gmail.com>
2025-03-15 11:47:34 +0200

    Add header links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+
+import { Routes, Route } from 'react-router'
+
 import CreateGame from './components/create-game/CreateGame'
 import EditGame from './components/edit-game/EditGame'
 import GameCatalog from './components/game-catalog/GameCatalog'
@@ -15,18 +18,19 @@ function App() {
 
       <Header />
 
-      <main id="main-content">
-        <Home />
+      <main id="main-content"> 
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/games' element={<GameCatalog />} />
+          <Route path='/games/create' element={<CreateGame />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes> 
 
-        <Login />
-        <Register />
+        {/* <EditGame /> */}
 
-        <CreateGame />
-        <EditGame />
+        {/* <GameDetails /> */}
 
-        <GameDetails />
-
-        <GameCatalog />
       </main>
 
     </div>
